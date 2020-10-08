e87a1ab450d96794123226905076f3b51975bef7
Ivan6274 <Isivanov2011@gmail.com>
2020-10-08 07:56:33 +0300

    Fix commit

---
+++ src/main/java/ru/netology/statistica/Statistica.java
@@ -0,0 +1,70 @@
+package ru.netology.statistica;
+
+public class Statistica {
+    public long calculateSum(long[] purchases) {
+        long sum = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+        }
+        return sum;
+    }
+    public long calculateAverageSells(long[] purchases) {
+        long sum = 0;
+        long avSells = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+            avSells = (sum / purchases.length);
+        }
+        return avSells;
+    }
+    public int calculateIndexOfMinSells(long[] purchases) {
+        int indexOfMin = 0;
+        long min = purchases[0];
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] < purchases[0]) {
+                min = purchases[i];
+                indexOfMin = i + 1;
+            }
+        }
+        return indexOfMin;
+    }
+    public int calculateIndexOfMaxSells (long[] purchases){
+        int indexOfMax = 0;
+        long max = purchases[0];
+        for (int i = 0; i < purchases.length - 1; i++) {
+            if (purchases[i] > purchases[i + 1]) {
+                max = purchases[i];
+                indexOfMax = i + 1;
+            }
+        }
+        return indexOfMax;
+    }
+    public int calculateMinSumSellsIndex (long[] purchases){
+        int minSumSellIndex = 0;
+        int sum = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+        }
+            int avSells = (sum/ purchases.length);
+            for (int i = 0; i < purchases.length; i++) {
+                if (avSells > purchases[i]) {
+                    minSumSellIndex++;
+                }
+            }
+        return minSumSellIndex;
+        }
+    public int calculateMaxSumSellsIndex (long[] purchases){
+        int maxSumSellIndex = 0;
+        int sum = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+        }
+        int avSells = (sum/ purchases.length);
+        for (int i = 0; i < purchases.length; i++) {
+            if (avSells < purchases[i]) {
+                maxSumSellIndex++;
+            }
+        }
+        return maxSumSellIndex;
+    }
+    }
---
+++ src/test/java/ru/netology/statistica/StatisticaTest.java
@@ -0,0 +1,59 @@
+package ru.netology.statistica;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatisticaTest {
+
+
+    @Test
+    public void calculateSum() {
+        Statistica service = new Statistica();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.calculateSum(purchases);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateAverageSells() {
+        Statistica service = new Statistica();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.calculateAverageSells(purchases);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void calculateIndexOfMinSells() {
+        Statistica service = new Statistica();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+        long actual = service.calculateIndexOfMinSells(purchases);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void calculateIndexOfMaxSells() {
+        Statistica service = new Statistica();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.calculateIndexOfMaxSells(purchases);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void calculateMinSumSellsIndex() {
+        Statistica service = new Statistica();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 4;
+        long actual = service.calculateMinSumSellsIndex(purchases);
+        assertEquals(expected, actual); c
+    }
+    @Test
+    public void calculateMaxSumSellsIndex() {
+        Statistica service = new Statistica();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 6;
+        long actual = service.calculateMaxSumSellsIndex(purchases);
+        assertEquals(expected, actual);
+    }
+}
